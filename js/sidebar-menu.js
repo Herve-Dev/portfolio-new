693a2e9acf0e44a0403148cb588ffd14e08074ba
Hervé Taillefond <madinina972represent@gmail.com>
2022-08-18 00:14:46 +0200

    add project skill about

--- js/sidebar-menu.js
+++ js/sidebar-menu.js
@@ -1,13 +1,13 @@
 function activeSideMenu () {
     const burgerMenu = document.querySelector('header > div')
     burgerMenu.classList.add('active')
-    burgerMenu.classList.remove('closeMenu')
+    burgerMenu.classList.remove('sidemenu-disable')
 }
 
 function closeSideMenu() {
     const sideIsActive = document.querySelector('.active');
         if (sideIsActive) {
             sideIsActive.classList.remove('active')
-           sideIsActive.classList.add('closeMenu') 
+            sideIsActive.classList.add('sidemenu-disable') 
         }
 }

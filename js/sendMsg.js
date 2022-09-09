5d47870ad88b36c754ef73476a2146859b33b1a6
Hervé Taillefond <madinina972represent@gmail.com>
2022-09-10 00:27:57 +0200

    work on nodemailer

--- js/sendMsg.js
+++ js/sendMsg.js
@@ -1,9 +1,23 @@
 const apiUrl = "http://localhost:3000/api/sendMessage";
-const emailContact = document.querySelector('.input-email');
-const msgContact = document.querySelector('.input-message');
 
-console.log(emailContact, msgContact);
+function sendMsg(event) {
+    event.preventDefault();
+    const emailContact = document.querySelector('.input-email').value;
+    const msgContact = document.querySelector('.input-message').value;
+    const formReset = document.querySelector('.bloc-form-contact__form-contact').reset()
 
-function sendMsg() {
-    
+    if (!emailContact || !msgContact) {
+        return formReset
+    }
+
+    axios.post(apiUrl, {
+        email:emailContact,
+        message:msgContact,
+    })
+    .then((result) => {
+        console.log(result);
+        return formReset
+    }).catch((err) => {
+        console.log(err);
+    });
 }
